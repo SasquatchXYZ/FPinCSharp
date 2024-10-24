547f4499e44c3da9fa830faa3afcaa1ce6d53029
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-10-23 23:37:39 -0400

    Initial commit

---
+++ FPinCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+using LaYumba.Functional;
+using Name = System.String;
+using Greeting = System.String;
+using PersonalizedGreeting = System.String;
+
+var greet = (Greeting gr, Name name) => $"{gr}, {name}";
+
+Name[] names = { "Tristan", "Ivan" };
+
+names.Map(n => greet("Hello", n)).ForEach(WriteLine);
